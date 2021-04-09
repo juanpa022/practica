1ca08f67bedd5f69874407a495b57a212ab5d85d
juanpa022 <juanpa_galin@hotmail.com>
2021-04-08 23:50:06 -0300

    first commit

---
+++ tarea2.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdalign.h>
+#include <conio.h>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    cout <<"hola cholossss";
+    return 0;
+}
